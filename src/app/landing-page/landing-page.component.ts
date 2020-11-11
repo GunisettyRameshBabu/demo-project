6a277a61bacc6544594c18041cd51843f2c72f38
kolli.durgaprasad8000@gmail.com <kolli.durgaprasad8000@gmail.com>
2020-11-11 19:01:21 +0530

    add

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  searchText;
+
+  searchItems: any[] = [
+    {id: "1", name: "EPay"},
+    {id: "2", name: "ATP"},
+    {id: "3", name: "JDE"},
+    {id: "4", name: "Invoice Tracking"},
+    {id: "5", name:'HR Portal'},
+    {id: "6", name:'Medical Benefits'},
+    {id: "7", name:'Customer Portal'},
+    {id: "8", name:'Time Card Tracking'},
+    {id: "9", name:'Mentoring'},
+    {id: "10", name:'Budget Tracking'},
+  ]
+
+  secondArray = []
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  textSearch(searchText) {
+    console.log('searchText', searchText)
+    this.secondArray = this.searchItems.filter(search => search.includes(searchText))
   }
 
 }
